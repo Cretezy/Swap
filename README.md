68bccc81452a327575f677245f053cecb583fd07
CraftThatBlock <craftthatblock@gmail.com>
2014-08-11 16:31:23 -0400

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ SwapUbuntu
 ==========
 
 Simple swap setup script for Ubuntu (14.04)
+
+Usage
+-----
+
+First of all, download the main script:
+```
+wget https://raw.githubusercontent.com/CraftThatBlock/SwapUbuntu/master/swap.sh -O swap
+```
+
+Then simply run the file with this format:
+```
+sh swap {size}
+```
+
+Example (2G is enough usually):
+```
+sh swap 2G
+```
+
+The default path for the swap file is /swapfile. If you wish to change this, simple the file location (FILE MUST NOT EXIST) add it to the command:
+```
+sh swap 2G /swapfile
+```
---
+++ swap.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Do argument checks
+if [ ! "$#" -ge 1 ]; then
+    echo "Usage: $0 {size}"
+    echo "Example: $0 2G"
+    echo "Optional path: Usage: $0 {size} {path}"
+    exit 1
+fi
+
+
+# Messages
+echo "=========================================================="
+echo "Welcome to CraftThatBlock's Ubuntu Swap install script!"
+echo "This script will automatically setup a swap file,"
+echo "install it, and do everything else needed."
+echo "All you have to do is enter your password and hit enter!"
+echo "=========================================================="
+echo ""
+
+# Setup variables
+SWAP_SIZE=$1
+SWAP_PATH="/swapfile"
+if [ ! -z "$2" ]; then
+    SWAP_PATH=$2
+fi
+
+
+# Start script
+sudo fallocate -l $SWAP_SIZE $SWAP_PATH
+sudo chmod 600 $SWAP_PATH
+sudo mkswap $SWAP_PATH
+sudo swapon $SWAP_PATH
+echo "/swapfile   none    swap    sw    0   0" | sudo tee /etc/fstab -a
+sudo sysctl vm.swappiness=10
+echo "vm.swappiness=10" | sudo tee /etc/sysctl.conf -a
+sudo sysctl vm.vfs_cache_pressure=50
+echo "vm.vfs_cache_pressure=50" | sudo tee /etc/sysctl.conf -a
+
+
+# Done
+echo ""
+echo "=========================================================="
+echo "Done! The apply these changes you simply have to restart:"
+echo "sudo reboot now"
+echo "=========================================================="
+echo ""
+
+
