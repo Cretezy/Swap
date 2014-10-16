b6fcc1bdc26489d3c0db7390afede3b98f770627
Sergey Vilgelm <sergey@vilgelm.info>
2014-10-16 09:57:49 +0200

    Update swap.sh
    use a SWAP_PATH instead of hardcoded /swapfile for writing into /etc/fstab

--- swap.sh
+++ swap.sh
@@ -31,7 +31,7 @@ sudo fallocate -l $SWAP_SIZE $SWAP_PATH
 sudo chmod 600 $SWAP_PATH
 sudo mkswap $SWAP_PATH
 sudo swapon $SWAP_PATH
-echo "/swapfile   none    swap    sw    0   0" | sudo tee /etc/fstab -a
+echo "$SWAP_PATH   none    swap    sw    0   0" | sudo tee /etc/fstab -a
 sudo sysctl vm.swappiness=10
 echo "vm.swappiness=10" | sudo tee /etc/sysctl.conf -a
 sudo sysctl vm.vfs_cache_pressure=50
